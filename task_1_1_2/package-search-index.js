3aee24e7b74c06da8d4bb551eeceb8e3848362c8
vlasival <vlasival@users.noreply.github.com>
2023-09-27 18:13:16 +0000

    Deploying to gh-pages from @ vlasival/OOP@f5214dc9faf11eb17df9faf51dfe66115044751c 🚀

--- task_1_1_2/package-search-index.js
+++ task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"task_1_1_2"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"task2"}];updateSearchResults();
--- task_1_1_2/type-search-index.js
+++ task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task_1_1_2","l":"Polynomial"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task2","l":"Polynomial"}];updateSearchResults();
