bd2f0e38604b42cc4e495e83887ff639da7d12ce
vlasival <vlasival@users.noreply.github.com>
2023-10-03 02:47:50 +0000

    Deploying to gh-pages from @ vlasival/OOP@67851e0152d5342725c0f1434c61c329105b0c25 🚀

--- task_1_1_2/type-search-index.js
+++ task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task","l":"Main"},{"p":"task","l":"Polynomial"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task","l":"Polynomial"}];updateSearchResults();
