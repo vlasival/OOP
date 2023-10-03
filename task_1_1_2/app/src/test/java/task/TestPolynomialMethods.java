ab57edb822b2d71849ba3ff350f6e0951269ca6a
vlasival <vlasival@gmail.com>
2023-10-03 09:53:56 +0700

    I'm burning like a fire gone wild on Saturday

--- task_1_1_2/app/src/test/java/task/TestPolynomialMethods.java
+++ task_1_1_2/app/src/test/java/task/TestPolynomialMethods.java
@@ -3,8 +3,6 @@ package task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
