5855e7b8771e64c6023e60ce45469f667b1e81b8
vlasival <vlasival@users.noreply.github.com>
2023-10-05 10:56:05 +0000

    Deploying to gh-pages from @ vlasival/OOP@e9ca7722a5f49be82c8020ba2f7dd6ff03d98cde 🚀

--- task_1_1_2/type-search-index.js
+++ task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task","l":"Polynomial"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"task","l":"ExcludeFromJacocoGeneratedReport"},{"p":"task","l":"Polynomial"}];updateSearchResults();
