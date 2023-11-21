3901b8a79cc62df53f870a3fa5cf0d6ec145c4aa
vlasival <vlasival@gmail.com>
2023-11-21 20:33:05 +0700

    Exception file

--- task_1_4_1/app/src/main/java/task/StudentBook.java
+++ task_1_4_1/app/src/main/java/task/StudentBook.java
@@ -5,7 +5,30 @@ import java.util.ArrayList;
 
 public class StudentBook {
     private ArrayList<MarkPage> pages = new ArrayList<>();
-    public int 
+    private int currentSemester;
+    private String studentName;
+
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        String[] nameParts = studentName.split(" ");
+        // checking that the string doesn't contain numbers and comprices 3 words.
+        if (nameParts.length != 3) {
+            throw new 
+        }
+        if (studentName.matches(".*\\d.*")) {
+
+        }
+        this.studentName = studentName;
+    }
+    public int getCurrentSemester() {
+        return currentSemester;
+    }
+    public void setCurrentSemester(int currentSemester) {
+        this.currentSemester = currentSemester;
+    }
+    
 
 }
 
---
+++ task_1_4_1/app/src/main/java/task/StudentBookException.java
@@ -0,0 +1,8 @@
+package task;
+
+public class StudentBookException extends Exception {
+    super(message);
+    public StudentBookException(String message) {
+        
+    }
+}
