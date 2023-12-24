8d1d3b76ddfe82226df5e14b5230d6968015a86a
vlasival <vlasival@users.noreply.github.com>
2023-12-24 19:14:55 +0000

    Deploying to gh-pages from @ vlasival/OOP@eed49d28d80b90cc116208aef071f5eaf5521d72 🚀

--- task_1_2_2/package-search-index.js
+++ task_1_2_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"task.entryPoint"},{"l":"task.exceptions"},{"l":"task.graphModel"},{"l":"task.graphs"},{"l":"task.resources"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"task.entrypoint"},{"l":"task.exceptions"},{"l":"task.graphmodel"},{"l":"task.graphs"},{"l":"task.resources"}];updateSearchResults();
